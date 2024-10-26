ed695a448392e6c505f195d7f6a1e013151c4af6
salihayoubi23 <salihayoubi23@outlook.com>
2024-10-26 04:07:26 +0200

    vv

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -8,7 +8,7 @@ router.get('/', authMiddleware, reservationController.getAllReservations);
 router.get('/user', authMiddleware, reservationController.getUserReservations);
 router.get('/user/paid', authMiddleware, reservationController.getUserPaidReservations);
 router.get('/:id', authMiddleware, reservationController.getReservationById);
-router.put('/:id/status', authMiddleware, reservationController.updateReservationStatus);
+router.put('/:id/status', authMiddleware, reservationController.updateReservationStatusToPaid);
 router.post('/create-checkout-session', authMiddleware, reservationController.createCheckoutSession);
 router.post('/webhook', express.raw({ type: 'application/json' }), reservationController.handleStripeWebhook);
 
