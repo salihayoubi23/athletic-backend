a056b63ed8ab3a6577f4b73257600af49b7374fa
salihayoubi23 <salihayoubi23@outlook.com>
2024-10-27 23:19:56 +0100

    jjj

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -4,7 +4,7 @@ const reservationController = require('../controllers/reservationController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/', authMiddleware, reservationController.createReservation);
-router.get('/user/paid', authMiddleware, getUserReservations);
+router.get('/user/paid', authMiddleware, reservationController.getUserReservations);
 router.get('/:id', authMiddleware, reservationController.getReservationById);
 router.put('/:id/status', authMiddleware, reservationController.updateReservationStatus);
 router.post('/create-checkout-session', authMiddleware, reservationController.createCheckoutSession);
